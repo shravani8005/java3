e30abfa02bd044dedf082186fbeef76806fe6100
shravani8005/ <shravani8005@gmail.com>
2025-04-23 02:06:35 -0700

    master

--- java/demo.java
+++ java/demo.java
@@ -3,6 +3,11 @@ class demo{
         int a=20; int b=40;
         System.out.println("sum is" + (a+b));
         System.out.println("subtraction is" + (a-b));
+        System.out.println("multiplication is" + (a*b));
+        System.out.println("div is" + (a/b));
+
+
+        
 
     }
 }

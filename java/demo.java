a77707e302c6b422d6ebdb5ec8b807b301256a39
shravani8005/ <shravani8005@gmail.com>
2025-04-23 01:57:55 -0700

    main

---
+++ java/demo.java
@@ -0,0 +1,8 @@
+class demo{
+    public static void main(String[] args) {
+        int a=20; int b=40;
+        System.out.println("sum is" + (a+b));
+        System.out.println("subtraction is" + (a-b));
+
+    }
+}
